06b435f0352c8455b328c59a9efa1172d77c56d1
Ringo Leese <ringo19@gmx.de>
2014-02-16 17:12:06 +0100

    update

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -17,8 +17,8 @@ CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/libc-trim.so.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
-CND_ARTIFACT_NAME_Release=liblibc-trim.so
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/liblibc-trim.so
+CND_ARTIFACT_NAME_Release=libc-trim.so
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/libc-trim.so
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Release=libc-trim.so.tar
 CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/libc-trim.so.tar
